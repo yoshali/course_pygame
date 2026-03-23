3eba2e780faf478decf6c34593e6ea030841f749
yoshali <yoshali@yandex.com>
2026-03-23 11:07:31 +0300

    Change movement mechanics

--- less1_game.py
+++ less1_game.py
@@ -28,6 +28,8 @@ running = True
 #Дельта-тайм - разница во времени в милисекундах между предыдущим и текущим кадром.
 dt = 0
 
+target_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
+
 #Позиция игрока в формате Vector2. Теперь player.pos хранит Vector2 объект, который запоминает координаты player
 #screen.get_width() screen.get_height() получают размеры экрана
 #Чтобы игрок со старта был в центре экрана любого размера, делим эти показатели на 2
@@ -47,28 +49,23 @@ while running:
             running = False
         
         #Если событие - это нажатие кнопки мыши
-        if event.type == pygame.MOUSEBUTTONDOWN:  # Кнопка мыши нажата
-            print(f"Нажата кнопка: {event.button}")
-            # 1-левая, 2-средняя, 3-правая, 4-колесо вверх, 5-колесо вниз
-            print(f"Позиция: {event.pos}")
-
-        '''if event.type == pygame.MOUSEBUTTONUP:  # Кнопка мыши отпущена
-            print(f"Отпущена кнопка: {event.button}")
-            print(f"Позиция: {event.pos}")'''
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:  # Кнопка мыши нажата
+             target_pos = event.pos
+            
     
     #Рисуем кружок игрока
     pygame.draw.circle(screen, "red", player_pos, 40)
 
-    #Передвигаем на клавиши мыши, пока не упираемся в края экрана
-    key = pygame.key.get_pressed()
-    if key[pygame.K_w]:
-        if player_pos.y >= 300 * dt: player_pos.y -= 300 * dt
-    if key[pygame.K_s]:
-        if player_pos.y <= screen.get_height() - 300 * dt: player_pos.y += 300 * dt
-    if key[pygame.K_a]:
-        if player_pos.x >= 300 * dt: player_pos.x -= 300 * dt
-    if key[pygame.K_d]:
-        if player_pos.x <= screen.get_width() - 300 * dt: player_pos.x += 300 * dt
+    if event.type == pygame.MOUSEBUTTONUP:  # Кнопка мыши отпущена
+            print(f"Отпущена кнопка: {event.button}")
+            print(f"Позиция: {event.pos}")
+
+    if target_pos:
+        direction = (target_pos - player_pos)
+    if direction.length() < 5:
+        target_pos = None
+    else:
+        player_pos += direction.normalize() * 300 * dt
 
 
     #Отображаем изображение из буфера (все то, что расовали до этого через blit по слоям)
